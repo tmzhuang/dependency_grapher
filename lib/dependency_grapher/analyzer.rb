5e512ea9710bfb073772fc490ce758b04a5860b2
tmzhuang <abstract.wolf@gmail.com>
2016-02-08 17:15:18 -0500

    Test

--- lib/dependency_grapher/analyzer.rb
+++ lib/dependency_grapher/analyzer.rb
@@ -1,15 +1,45 @@
 module DependencyGrapher
-	require 'graphviz'
+	require 'set'
 	class Analyzer
-		def initialize(dependencies, clusters)
+		def initialize(dependencies, *filters)
 			@dependencies = dependencies
-			@clusters = clusters
 
+      # Crawl folders for class names 
 			@controllers = classes_in("controllers")
 			@models = classes_in("models")
 			@services = classes_in("services")
+
+      # Add default filters
+			if filters.empty?
+        add_filters(:interclass, :rails)
+      # Otherwise, add provided filters
+      else
+        add_filters(filters)
+      end
+
+      #Apply filters
+      apply_filters
 		end
 
+		def add_filters(*filters)
+      @filters.merge filters
+		end
+
+		def remove_filters(*filters)
+		end
+
+    def apply_filters
+      @filters.each do |filter|
+        case filter
+        when :intercluster
+        when :rails
+      end
+    end
+
+    def calc_dependencies
+    end
+
+    private
 		# Returns an array of class names in folder specified by argument. Since we are dealing 
 		# with a Rails app, folder is expected to be in app/. For example, if folder is specified
 		# to be 'models', then classes in 'app/models/' will be returned.
@@ -19,48 +49,9 @@ module DependencyGrapher
 				result << file[/app\/#{folder}\/(.*)\.rb/, 1].camelize
 			end
 		end
-
-		def apply_filter(*filters)
-			if filters.size > 1
-				filters.map(&:apply_filter)
-				filters.each do |filter|
-					apply_filter(filter)
-				end
-			else
-				case filters.first
-				when :interclass
-				when :rails
-				end
-			end
-		end
 		
-		def remove_filter(*filters)
-		end
-
 		def calc_filter_set(filter)
 		end
 
-		def graph
-			# Initialize digraph
-			graph = GraphViz.digraph( :G, type: :digraph)
-			clusters = {}
-			# Create clusters
-			@clusters.each do |aclass|
-				clusters[aclass] = graph.add_graph("cluster_" + aclass.to_s)
-				clusters[aclass][:label] = aclass.to_s
-			end
-			#Add nodes and edges
-			@dependencies.each do |dependency| 
-				dependency.each do |acaller, acallee|
-					calling_method_name = acaller[:method].inspect
-					callee_method_name = acallee[:method].inspect
-					clusters[acaller[:class]].add_nodes(calling_method_name)
-					clusters[acallee[:class]].add_nodes(callee_method_name)
-					graph.add_edges(calling_method_name, callee_method_name)
-				end
-			end 
-			# Generate graph
-			graph.output( png: "graph.png")
-		end
 	end
 end
