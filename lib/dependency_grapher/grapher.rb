1a3e7398edaeef813cecb5bbd5c7e64baba6252a
tmzhuang <abstract.wolf@gmail.com>
2016-02-08 18:00:40 -0500

    Test

--- lib/dependency_grapher/grapher.rb
+++ lib/dependency_grapher/grapher.rb
@@ -1,4 +1,33 @@
 module DependencyGrapher
+	require 'graphviz'
 	class Grapher
+    def initialize(dependencies)
+      @analyzer = Analyzer.new(dependencies)
+    end
+
+		def graph
+      p "hi"
+			## Initialize digraph
+			#graph = GraphViz.digraph( :G, type: :digraph)
+			#clusters = {}
+			## Create clusters
+			#@clusters.each do |aclass|
+				#clusters[aclass] = graph.add_graph("cluster_" + aclass.to_s)
+				#clusters[aclass][:label] = aclass.to_s
+			#end
+
+			##Add nodes and edges
+			#@dependencies.each do |dependency| 
+				#dependency.each do |acaller, acallee|
+					#calling_method_name = acaller[:method].inspect
+					#callee_method_name = acallee[:method].inspect
+					#clusters[acaller[:class]].add_nodes(calling_method_name)
+					#clusters[acallee[:class]].add_nodes(callee_method_name)
+					#graph.add_edges(calling_method_name, callee_method_name)
+				#end
+			#end 
+			## Generate graph
+			#graph.output( png: "graph.png")
+		end
 	end
 end
