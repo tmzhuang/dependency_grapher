10e2c61ad7fe1f3e2763d8b52023f3e96f363785
tmzhuang <abstract.wolf@gmail.com>
2016-02-08 17:53:23 -0500

    Missing end statement

--- lib/dependency_grapher/analyzer.rb
+++ lib/dependency_grapher/analyzer.rb
@@ -1,38 +1,39 @@
 module DependencyGrapher
-	require 'set'
-	class Analyzer
-		def initialize(dependencies, *filters)
-			@dependencies = dependencies
+  require 'set'
+  class Analyzer
+    def initialize(dependencies, *filters)
+      @dependencies = dependencies
 
       # Crawl folders for class names 
-			@controllers = classes_in("controllers")
-			@models = classes_in("models")
-			@services = classes_in("services")
+      @controllers = classes_in("controllers")
+      @models = classes_in("models")
+      @services = classes_in("services")
 
       # Add default filters
-			if filters.empty?
+      if filters.empty?
         add_filters(:interclass, :rails)
-      # Otherwise, add provided filters
+        # Otherwise, add provided filters
       else
         add_filters(filters)
       end
 
       #Apply filters
       apply_filters
-		end
+    end
 
-		def add_filters(*filters)
+    def add_filters(*filters)
       @filters.merge filters
-		end
+    end
 
-		def remove_filters(*filters)
-		end
+    def remove_filters(*filters)
+    end
 
     def apply_filters
       @filters.each do |filter|
         case filter
         when :intercluster
         when :rails
+        end
       end
     end
 
@@ -40,18 +41,18 @@ module DependencyGrapher
     end
 
     private
-		# Returns an array of class names in folder specified by argument. Since we are dealing 
-		# with a Rails app, folder is expected to be in app/. For example, if folder is specified
-		# to be 'models', then classes in 'app/models/' will be returned.
-		def classes_in(folder)
-			result = []
-			Dir.glob("app/#{folder}/*.rb").map do |file|
-				result << file[/app\/#{folder}\/(.*)\.rb/, 1].camelize
-			end
-		end
-		
-		def calc_filter_set(filter)
-		end
+    # Returns an array of class names in folder specified by argument. Since we are dealing 
+    # with a Rails app, folder is expected to be in app/. For example, if folder is specified
+    # to be 'models', then classes in 'app/models/' will be returned.
+    def classes_in(folder)
+      result = []
+      Dir.glob("app/#{folder}/*.rb").map do |file|
+        result << file[/app\/#{folder}\/(.*)\.rb/, 1].camelize
+      end
+    end
 
-	end
+    def calc_filter_set(filter)
+    end
+
+  end
 end
