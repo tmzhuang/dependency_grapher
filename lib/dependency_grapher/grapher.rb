23deed609904b016b2a0d709b99e798e06981640
tmzhuang <abstract.wolf@gmail.com>
2016-02-09 22:37:47 -0500

    Think I fixed the bug

--- lib/dependency_grapher/grapher.rb
+++ lib/dependency_grapher/grapher.rb
@@ -12,21 +12,15 @@ module DependencyGrapher
       @dependencies.each do |dependency|
         caller = dependency.caller
         receiver = dependency.receiver
-        p "Adding dep: #{caller} to #{receiver}"
-        ##p "Okay wtf is going on.."
-        #p "caller class is #{caller.defined_class}"
-        #p "\tas an array it is #{caller.defined_class.to_s.split("::")}"
-        # Array containing name of classes
-        # ex. ["Minitest", "Test"]
         # Create clusters
-        #p "Creating caller classes: #{caller_classes}"
         create_clusters_from(caller.defined_class)
-        #p "Creating receiver classes: #{receiver_classes}"
         create_clusters_from(receiver.defined_class)
         # Create nodes
-        calling_node = create_node_from(caller)
-        receiving_node = create_node_from(receiver)
+        create_node_from(caller)
+        create_node_from(receiver)
         # Create edge
+        calling_node = caller.method_id.to_s
+        receiver_node = caller.method_id.to_s
         @graph.add_edges(calling_node, receiving_node)
       end
 
@@ -41,31 +35,23 @@ module DependencyGrapher
       klass.to_s.split("::")
     end
 
-    # Given a depth and class_name, returns the subgraph from the @clusters
-    # instance variable at that location
-    def get_cluster(depth, class_name)
-      #p "clusters[#{depth}][#{class_name}] #{@clusters[depth][class_name]}"
-      @clusters[depth][class_name]
-    end
-
     # Given a method, adds it to the cluster its class belongs to
     def create_node_from(method)
       classes = class_to_a(method.defined_class)
-      cluster = get_cluster(classes.size - 1, classes.last)
-      #p "Cluster: #{cluster}"
+      depth = classes.size - 1
+      class = classes.last
+      #cluster = get_cluster(classes.size - 1, classes.last)
       cluster.add_nodes(method.method_id.to_s)
-      ;
     end
 
     # Given an array of classes, creates clusters from the array
     def create_clusters_from(klass)
       # Split classes into an array 
-      classes = klass.to_s.split("::")
+      classes = class_to_a(klass)
       # Iterate over array and add each element as a subgraph at corresponding depth
       classes.each_with_index do |klass, i|
         @clusters[i] ||= {} 
         curr_class = classes[i]
-        #p "curr_class #{ curr_class }"
         if i == 0
           # If we're at the root, add the cluster to the graph
           subgraph = @graph.add_graph("cluster_" + curr_class)
@@ -79,7 +65,6 @@ module DependencyGrapher
           #Otherwise add it as a subgraph of the previous class
           prev_class = classes[i-1]
           @clusters[i][curr_class] = @clusters[i-1][prev_class].add_graph("cluster_" + curr_class)
-          #p "prev_class #{ prev_class }"
         end
         # Label current graph
         @clusters[i][curr_class][:label] = curr_class
